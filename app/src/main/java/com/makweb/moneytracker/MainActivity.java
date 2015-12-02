7d83651811c147c00b0c9bd6e825bacec811e5c9
makweb <makweb@yandex.ru>
2015-12-02 19:23:37 +0400

    add snackbar + LOG lifecycle activity

--- app/src/main/java/com/makweb/moneytracker/MainActivity.java
+++ app/src/main/java/com/makweb/moneytracker/MainActivity.java
@@ -1,8 +1,12 @@
 package com.makweb.moneytracker;
 
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 
@@ -10,14 +14,29 @@ public class MainActivity extends AppCompatActivity {
 
     final String TAG="main activity";
     private TextView txtHelloText;
+    private Button btnShowToast;
+    private CoordinatorLayout clayMainActivity;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        clayMainActivity = (CoordinatorLayout)findViewById(R.id.coordinatorLayout);
         txtHelloText = (TextView)findViewById(R.id.textHello);
+        btnShowToast = (Button)findViewById(R.id.button);
+        btnShowToast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar snackbar = Snackbar
+                        .make(clayMainActivity, "my first snack", Snackbar.LENGTH_LONG);
+                snackbar.show();
+            }
+        });
+
         Log.d(TAG, "MainActivity: onCreate()");
+
     }
 
     @Override
